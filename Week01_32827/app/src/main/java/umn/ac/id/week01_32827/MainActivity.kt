7299969dc7930371b44b7b447c82b4925a4bb2b6
lauslim12 <nicholasdwiarto@yahoo.com>
2021-01-27 19:59:48 +0700

    Add lab module 1

---
+++ Week01_32827/app/src/main/java/umn/ac/id/week01_32827/MainActivity.kt
@@ -0,0 +1,12 @@
+package umn.ac.id.week01_32827
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView // Unused for now.
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
