76a832a347b7b3fd200e713c064fd49b8e6e7275
lauslim12 <nicholasdwiarto@yahoo.com>
2021-01-29 11:39:07 +0700

    Readd module 01 with Java

---
+++ Week01_32827/app/src/main/java/umn/ac/id/week01_32827/MainActivity.java
@@ -0,0 +1,13 @@
+package umn.ac.id.week01_32827;
+
+import androidx.appcompat.app.AppCompatActivity;
+// import androidx.recyclerview.widget.RecyclerView; Unused for now.
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
