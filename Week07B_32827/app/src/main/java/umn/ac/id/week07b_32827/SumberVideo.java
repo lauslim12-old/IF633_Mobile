c94e1a777ed3156bd16f21524d9a631fecaa3e27
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-14 15:58:37 +0700

    Add lab module 07

---
+++ Week07B_32827/app/src/main/java/umn/ac/id/week07b_32827/SumberVideo.java
@@ -0,0 +1,43 @@
+package umn.ac.id.week07b_32827;
+
+import java.io.Serializable;
+
+public class SumberVideo implements Serializable {
+    private String judul;
+    private String keterangan;
+    private String videoUri;
+
+    public SumberVideo(String judul, String keterangan, String videoUri) {
+        this.judul = judul;
+        this.keterangan = keterangan;
+        this.videoUri = videoUri;
+    }
+
+    public String getJudul() {
+        return this.judul;
+    }
+
+    public String getKeterangan() {
+        return this.keterangan;
+    }
+
+    public String getVideoUri() {
+        return this.videoUri;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public void setVideoUri(String videoUri) {
+        this.videoUri = videoUri;
+    }
+
+    public String toString() {
+        return this.getJudul() + "=>" + this.getKeterangan();
+    }
+}
